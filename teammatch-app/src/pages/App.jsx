d06840389b1110965361ea15fa84037f54e9a4be
2toria <stepanovatoria@gmail.com>
2025-04-06 18:45:24 +0300

    Add files via upload

---
+++ teammatch-app/src/pages/App.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>👾 TeamMatch</h1>
+      <p>Поиск партнёров и команд для игр</p>
+    </div>
+  )
+}
